c2091dea423c6c0e20ff9ea82f5b2e65f71485d6
Pranjal jain <pj2991994@yahoo.com>
2019-04-22 18:35:31 +0530

    Login and registration with password reset
    This is login and registration page with password reset link using django. registration app have login, registration and password reset logic using django inbuilt methods.

--- registration/urls.py
+++ registration/urls.py
@@ -1,10 +1,8 @@
-from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
+	path('',include('django.contrib.auth.urls')),
     path('register/', views.register,name='register'),
-	path('login/', LoginView.as_view(), name='login'),
 	path('profile/', views.profile , name='profile'),
-	path('logout/', LogoutView.as_view(), name='logout'),
 ]
